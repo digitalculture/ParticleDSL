414e73c58e40da8487b4b289fa3ae4de62e081cc
Christian Ludborzs <mai03fyv@studserv.uni-leipzig.de>
2014-06-09 22:40:14 +0200

    Parameters for dambreak experiment

--- dambreak/src/main/scala/de/digitalculture/sph/dambreak/Dambreak.scala
+++ dambreak/src/main/scala/de/digitalculture/sph/dambreak/Dambreak.scala
@@ -22,18 +22,18 @@ object Dambreak {
   val minv = (0.0, 0.0)
   val maxv = (width.toDouble, height.toDouble)
 
-  val dt = 0.1
-  val h = 1.1
-  val ips = 0.6 * h
+  val dt = 0.05
+  val h = 1.4
+  val ips = 0.4 * h
   val dens = new DensityEstimation(minXSqKernel, h)
   val rho0ip = NeighborModel.rho0(minXSqKernel, ips, h)
-  val k = 0.1
+  val k = 0.5
   val presest = new PressureEstimation(rho0ip, k)
   val pgrad = new PressureGradient(minXSqKernel, h, presest)
   val pimpulse = new PressureImpulse(pgrad)
-  val maxit = 50
-  val rhotol = 1.1 * rho0ip
-  val krelax = 1.5
+  val maxit = 100
+  val rhotol = 1.4 * rho0ip
+  val krelax = 0.8
   val simpRelax = new SimpleRelaxation(dens, minv, maxv, dt, krelax, rhotol, maxit)
   val visc = new DiffusionViscosity(GaussKernel, 0.8)
 
@@ -63,7 +63,7 @@ object Dambreak {
     val r2star: VectorList = advancePositions(r1, v1, minv, maxv, dt)
     val relax2: VectorList = simpRelax.relaxImpulse((r2star, nullimpulse))
     val r2: VectorList = advancePositions(r2star, relax2, minv, maxv, 1.0)
-    val v2: VectorList = r2 - r1
+    val v2: VectorList = (r2 - r1)/dt
     (r2, v2)
   }
 
